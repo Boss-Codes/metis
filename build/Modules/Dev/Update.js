8e0b08d3a80f6d9a8594529c9241ec5746e09014
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-22 07:46:33 +0000

    asfd

--- build/Modules/Dev/Update.js
+++ build/Modules/Dev/Update.js
@@ -16,7 +16,6 @@ class Update extends Command_1.Command {
             showOnHelp: false,
             deleteOnUsage: false,
             enabled: true,
-            aliases: ['build']
         });
     }
     async execute(metis, ctx) {
