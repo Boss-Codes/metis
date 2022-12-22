f8ecb766155640a26d1969d1ab15d4be37a7b9f9
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-22 07:34:41 +0000

    hopefully fix help cmd, doubt it tho

--- build/Modules/Info/Help.js
+++ build/Modules/Info/Help.js
@@ -23,28 +23,6 @@ class Help extends Command_1.Command {
         if (!ctx.args) {
             return ctx.channel.createMessage('**Commands List**\n <https://github.com/Boss-Codes/metis-ts/wiki/Commands>\n\n**Bot Support Server**\nhttps://discord.gg/mePghx6dQy');
         }
-        const error = {
-            embeds: [{
-                    color: metis.colors.red,
-                    description: `${metis.emotes.error} I could not find that command or module.`,
-                }],
-            flags: 64,
-            components: [{
-                    type: 1,
-                    components: [{
-                            type: 2,
-                            style: 5,
-                            label: "Command List",
-                            url: 'https://github.com/Boss-Codes/metis-ts/wiki/Commands'
-                        },
-                        {
-                            type: 2,
-                            style: 5,
-                            label: 'Support Server',
-                            url: 'https://discord.gg/mePghx6dQy'
-                        }],
-                }],
-        };
         const cmd = metis.commands.get(ctx.args[0]) || metis.commands.find(cmd => cmd.aliases && cmd.aliases.includes(ctx.args[0]));
         if (!cmd) {
             return ctx.channel.createMessage({
@@ -75,16 +53,37 @@ class Help extends Command_1.Command {
                 color: metis.colors.blue,
                 title: `${cmd.module.toLowerCase()}:${cmd.name}`,
                 description: cmd.description,
-                fields: [
-                    { name: 'Usage:', value: `\`${cmd.usage}\`` },
-                    { name: 'Examples:', value: `\`${cmd.example}\`` }
-                ]
+                fields: []
             }
         };
+        if (!cmd.usage) {
+            data.embed.fields.push({
+                name: 'Usage:',
+                value: `\`${cmd.usage}\``
+            });
+        }
+        if (cmd.usage) {
+            data.embed.fields.push({
+                name: 'Usage:',
+                value: `\`${cmd.name} ${cmd.usage}\``
+            });
+        }
+        if (cmd.example) {
+            data.embed.fields.push({
+                name: 'Example:',
+                value: `\`${cmd.name} ${cmd.example}\``
+            });
+        }
         if (cmd.aliases) {
             data.embed.fields.push({
                 name: 'Aliases:',
                 value: cmd.aliases.map(a => `\`${a}\``).join(", ")
+            });
+        }
+        if (cmd.permLevel === 0) {
+            data.embed.fields.push({
+                name: 'Permissions:',
+                vaule: 'User'
             });
         }
         if (cmd) {
--- src/Modules/Info/Help.ts
+++ src/Modules/Info/Help.ts
@@ -23,28 +23,6 @@ class Help extends Command {
         
         if (!ctx.args){return ctx.channel.createMessage('**Commands List**\n <https://github.com/Boss-Codes/metis-ts/wiki/Commands>\n\n**Bot Support Server**\nhttps://discord.gg/mePghx6dQy')}
         
-        const error = {
-            embeds: [{
-                color: metis.colors.red,
-                description: `${metis.emotes.error} I could not find that command or module.`,
-            }],
-            flags: 64,
-            components: [{
-                type: 1,
-                components: [{
-                    type: 2,
-                    style: 5,
-                    label: "Command List",
-                    url: 'https://github.com/Boss-Codes/metis-ts/wiki/Commands' 
-                }, 
-                {
-                    type: 2, 
-                    style: 5, 
-                    label: 'Support Server', 
-                    url: 'https://discord.gg/mePghx6dQy'
-                }],
-            }],
-        }
         const cmd = metis.commands.get(ctx.args[0]) || metis.commands.find(cmd => cmd.aliases && cmd.aliases.includes(ctx.args[0]))
         if (!cmd) {return ctx.channel.createMessage({ 
             embeds: [{
@@ -74,12 +52,29 @@ class Help extends Command {
                 title: `${cmd.module.toLowerCase()}:${cmd.name}`, 
                 description: cmd.description, 
                 fields: [
-                    { name: 'Usage:', value: `\`${cmd.usage}\``}, 
-                    { name: 'Examples:', value: `\`${cmd.example}\``}
                 ]
             }
         }
 
+        if (!cmd.usage) { 
+            data.embed.fields.push({
+                name: 'Usage:', 
+                value: `\`${cmd.usage}\``
+            })
+        } 
+        if (cmd.usage) { 
+            data.embed.fields.push({
+                name: 'Usage:', 
+                value: `\`${cmd.name} ${cmd.usage}\``
+            })
+        }
+
+        if (cmd.example) { 
+            data.embed.fields.push({
+                name: 'Example:', 
+                value: `\`${cmd.name} ${cmd.example}\``
+            })
+        }
 
         if (cmd.aliases) { 
             data.embed.fields.push({
@@ -87,6 +82,14 @@ class Help extends Command {
                 value: cmd.aliases.map(a => `\`${a}\``).join(", ")
             })
         }
+
+        if (cmd.permLevel === 0) { 
+            data.embed.fields.push({
+                name: 'Permissions:', 
+                vaule: 'User'
+            })
+        }
+
 
         if (cmd) { 
             ctx.channel.createMessage(data)
