aac73581e065f9b67794539a5810b4d1b738c709
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-24 03:58:14 +0000

    Add module support + qol changes

--- build/Modules/Info/Help.js
+++ build/Modules/Info/Help.js
@@ -8,8 +8,8 @@ class Help extends Command_1.Command {
             name: 'help',
             module: 'Info',
             description: 'Lists the bot\'s commands or gives information on a specific command.',
-            usage: 'help [command name]',
-            example: 'help serverinfo',
+            usage: '[command name]',
+            example: 'serverinfo',
             requiredGuilds: [],
             requiredUsers: [],
             deleteOnUsage: false,
@@ -24,6 +24,7 @@ class Help extends Command_1.Command {
             return ctx.channel.createMessage('**Commands List**\n <https://github.com/Boss-Codes/metis-ts/wiki/Commands>\n\n**Bot Support Server**\nhttps://discord.gg/mePghx6dQy');
         }
         const cmd = metis.commands.get(ctx.args[0]) || metis.commands.find(cmd => cmd.aliases && cmd.aliases.includes(ctx.args[0]));
+        const mCmds = metis.commands.filter(c => c.module === ctx.args[0]);
         if (!cmd) {
             return ctx.channel.createMessage({
                 embeds: [{
@@ -46,6 +47,54 @@ class Help extends Command_1.Command {
                                 url: 'https://discord.gg/mePghx6dQy'
                             }],
                     }],
+            });
+        }
+        if (ctx.args[0] === 'Dev' && !metis.developer.includes(ctx.user.id) || ctx.args[0] === 'dev' && !metis.developer.includes(ctx.user.id)) {
+            return ctx.channel.createMessage({
+                embeds: [{
+                        color: metis.colors.red,
+                        description: `${metis.emotes.error} I could not find that command or module.`,
+                    }],
+                flags: 64,
+                components: [{
+                        type: 1,
+                        components: [{
+                                type: 2,
+                                style: 5,
+                                label: "Command List",
+                                url: 'https://github.com/Boss-Codes/metis-ts/wiki/Commands'
+                            },
+                            {
+                                type: 2,
+                                style: 5,
+                                label: 'Support Server',
+                                url: 'https://discord.gg/mePghx6dQy'
+                            }],
+                    }]
+            });
+        }
+        if (!mCmds) {
+            return ctx.channel.createMessage({
+                embeds: [{
+                        color: metis.colors.red,
+                        description: `${metis.emotes.error} I could not find that command or module.`,
+                    }],
+                flags: 64,
+                components: [{
+                        type: 1,
+                        components: [{
+                                type: 2,
+                                style: 5,
+                                label: "Command List",
+                                url: 'https://github.com/Boss-Codes/metis-ts/wiki/Commands'
+                            },
+                            {
+                                type: 2,
+                                style: 5,
+                                label: 'Support Server',
+                                url: 'https://discord.gg/mePghx6dQy'
+                            }],
+                    }]
             });
         }
         if (cmd.module === 'Dev' && !metis.developer.includes(ctx.user.id)) {
@@ -72,20 +121,36 @@ class Help extends Command_1.Command {
                     }],
             });
         }
+        const module = {
+            embed: {
+                color: metis.colors.blue,
+                description: mCmds.map(c => `\`${cmd}\``).join('\n')
+            }
+        };
         const data = {
             embed: {
                 color: metis.colors.blue,
                 title: `${cmd.module.toLowerCase()}:${cmd.name}`,
                 description: cmd.description,
-                fields: [
-                    { name: 'Usage:', value: `\`${cmd.usage}\`` }
-                ]
+                fields: []
             }
         };
+        if (cmd.usage) {
+            data.embed.fields.push({
+                name: 'Usage:',
+                value: `\`${cmd.name} ${cmd.usage}\``
+            });
+        }
+        else {
+            data.embed.fields.push({
+                name: 'Usage:',
+                value: `\`${cmd.name}\``
+            });
+        }
         if (cmd.example) {
             data.embed.fields.push({
                 name: 'Example:',
-                value: `\`${cmd.example}\``
+                value: `\`${cmd.name} ${cmd.example}\``
             });
         }
         if (cmd.aliases) {
@@ -123,6 +188,9 @@ class Help extends Command_1.Command {
                 name: 'Permissions:',
                 value: 'Developer'
             });
+        }
+        if (mCmds) {
+            ctx.channel.createMessage(module);
         }
         if (cmd) {
             ctx.channel.createMessage(data);
--- src/Modules/Info/Help.ts
+++ src/Modules/Info/Help.ts
@@ -7,8 +7,8 @@ class Help extends Command {
             name: 'help', 
             module: 'Info', 
             description: 'Lists the bot\'s commands or gives information on a specific command.', 
-            usage: 'help [command name]', 
-            example: 'help serverinfo', 
+            usage: '[command name]', 
+            example: 'serverinfo', 
             requiredGuilds: [], 
             requiredUsers: [], 
             deleteOnUsage: false, 
@@ -24,6 +24,8 @@ class Help extends Command {
         if (!ctx.args){return ctx.channel.createMessage('**Commands List**\n <https://github.com/Boss-Codes/metis-ts/wiki/Commands>\n\n**Bot Support Server**\nhttps://discord.gg/mePghx6dQy')}
         
         const cmd = metis.commands.get(ctx.args[0]) || metis.commands.find(cmd => cmd.aliases && cmd.aliases.includes(ctx.args[0]))
+        const mCmds = metis.commands.filter(c => c.module === ctx.args[0]) 
+        
         if (!cmd) {return ctx.channel.createMessage({ 
             embeds: [{
                 color: metis.colors.red,
@@ -46,6 +48,54 @@ class Help extends Command {
                 }],
             }],
         })}
+        if (ctx.args[0] === 'Dev' && !metis.developer.includes(ctx.user.id) || ctx.args[0] === 'dev' && !metis.developer.includes(ctx.user.id)) { 
+            return ctx.channel.createMessage({ 
+                embeds: [{
+                    color: metis.colors.red,
+                    description: `${metis.emotes.error} I could not find that command or module.`,
+                }],
+                flags: 64,
+                components: [{
+                    type: 1,
+                    components: [{
+                        type: 2,
+                        style: 5,
+                        label: "Command List",
+                        url: 'https://github.com/Boss-Codes/metis-ts/wiki/Commands' 
+                    }, 
+                    {
+                        type: 2, 
+                        style: 5, 
+                        label: 'Support Server', 
+                        url: 'https://discord.gg/mePghx6dQy'
+                    }],
+                }]
+            })
+        }
+
+        if (!mCmds) { return ctx.channel.createMessage({
+            embeds: [{
+                color: metis.colors.red,
+                description: `${metis.emotes.error} I could not find that command or module.`,
+            }],
+            flags: 64,
+            components: [{
+                type: 1,
+                components: [{
+                    type: 2,
+                    style: 5,
+                    label: "Command List",
+                    url: 'https://github.com/Boss-Codes/metis-ts/wiki/Commands' 
+                }, 
+                {
+                    type: 2, 
+                    style: 5, 
+                    label: 'Support Server', 
+                    url: 'https://discord.gg/mePghx6dQy'
+                }],
+            }]
+        })}
+
         if (cmd.module === 'Dev' && !metis.developer.includes(ctx.user.id)) {return ctx.channel.createMessage({ 
             embeds: [{
                 color: metis.colors.red,
@@ -68,21 +118,40 @@ class Help extends Command {
                 }],
             }],
         })}
+
+        const module = { 
+            embed: { 
+                color: metis.colors.blue, 
+                description: mCmds.map(c => `\`${cmd}\``).join('\n')
+            }
+        }
+
         const data = { 
             embed: { 
                 color: metis.colors.blue, 
                 title: `${cmd.module.toLowerCase()}:${cmd.name}`, 
                 description: cmd.description, 
                 fields: [
-                    { name: 'Usage:', value: `\`${cmd.usage}\``}
+                    
                 ]
             }
         }
 
+        if (cmd.usage) { 
+            data.embed.fields.push({
+                name: 'Usage:', 
+                value: `\`${cmd.name} ${cmd.usage}\``
+            })
+        } else { 
+            data.embed.fields.push({
+                name: 'Usage:', 
+                value: `\`${cmd.name}\``
+            })
+        }
         if (cmd.example) { 
             data.embed.fields.push({
                 name: 'Example:', 
-                value: `\`${cmd.example}\``
+                value: `\`${cmd.name} ${cmd.example}\``
             })
         }
 
@@ -124,6 +193,9 @@ class Help extends Command {
             })
         }
 
+        if (mCmds) { 
+            ctx.channel.createMessage(module)
+        }
 
         if (cmd) { 
             ctx.channel.createMessage(data)
