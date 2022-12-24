b96cee2a3a491b8c7383a8b6aedca73bda4824d3
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-24 08:28:54 +0000

    fuck me upo

--- build/Modules/Dev/Eval.js
+++ build/Modules/Dev/Eval.js
@@ -3,8 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Command_1 = require("../../Core/Structures/Command");
 const types_1 = require("../../types");
 const util_1 = require("util");
-const console_1 = require("console");
-const console_1 = require("console");
 class Eval extends Command_1.Command {
     constructor() {
         super({
